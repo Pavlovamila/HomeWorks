21fb4d9c94f03cd34331c936684d01fe362db5b4
Lyudmila_Pavlova <mila_pavlova1991@mail.ru>
2022-10-27 23:27:49 +0500

    Seminar 1

---
+++ Seminar1/Program.cs
@@ -0,0 +1,14 @@
+﻿//ДЗ 1
+//Задача 1. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+
+Console.Write("Input a first number: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+int quad2 = n2* n2;
+
+if(quad2 == n1)
+    Console.WriteLine($"Number {n1} is a square of {n2}");
+else
+    Console.WriteLine($"Number {n1} is not a square of {n2}");
