53f0a974876607ca64ee688d3a646ae7d5060a57
Lyudmila_Pavlova <mila_pavlova1991@mail.ru>
2022-10-27 23:37:41 +0500

    Seminar 1 saving

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -1,6 +1,6 @@
 ﻿//ДЗ 1
 //Задача 1. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
-
+/*
 Console.Write("Input a first number: ");
 int n1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input a second number: ");
@@ -12,3 +12,42 @@ if(quad2 == n1)
     Console.WriteLine($"Number {n1} is a square of {n2}");
 else
     Console.WriteLine($"Number {n1} is not a square of {n2}");
+*/
+
+//Задача 2. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+/*
+Console.Write("Input a positive integer number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int current = -number;
+
+while(current <= number)
+{
+    Console.Write(current + " ");
+    current++;
+}
+*/
+
+// Задача 3. Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+/*
+Console.Write("Input three-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = number % 10;
+Console.WriteLine($"Last digit of {number} is {result}");
+*/
+
+//Задача 4. Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Input a first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for(int i = 0; i <= 0; i++)
+{
+    if(a % 2 == 0)
+    {
+        Console.WriteLine(a + " ");
+    }
+}
