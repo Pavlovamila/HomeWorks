801a91374738291aed9a4bffe9648245c3ef94c9
Lyudmila_Pavlova <mila_pavlova1991@mail.ru>
2022-10-28 00:14:47 +0500

    Seminar 2 save

---
+++ Seminar2/Program.cs
@@ -0,0 +1,28 @@
+﻿ //Задача 1.
+/*
+ int CutDigit(int number)
+{
+    int ed = number % 10;
+    int sot = number / 100;
+
+    int result = sot * 10 + ed;
+    return result;
+}
+
+int randNum = new Random(). Next(100, 1000);
+int newNum = CutDigit(randNum);
+
+Console.WriteLine($"New version of{randNum} is {newNum}");
+
+bool IsEven(int number)
+{
+    if(number % 2 == 0)
+        return true;
+    else 
+        return false;
+}
+
+bool result = IsEven(newNum);
+Console.WriteLine(result);
+*/
+
