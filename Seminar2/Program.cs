e52cf9f767ab81b1a581bafe6b88951256a39f72
Pavlovamila <115178959+Pavlovamila@users.noreply.github.com>
2022-11-04 00:13:04 +0500

    Update Program.cs
    save homework2

--- Seminar2/Program.cs
+++ Seminar2/Program.cs
@@ -1,4 +1,4 @@
-﻿ //Задача 1.
+﻿ //Задача 1.Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 /*
  int CutDigit(int number)
 {
@@ -26,3 +26,33 @@ bool result = IsEven(newNum);
 Console.WriteLine(result);
 */
 
+//Задача 2: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+/*
+Console.Write("Input a number: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+if (anyNumberText.Length > 2){
+  Console.WriteLine("third number -> " + anyNumberText[2]);
+}
+else {
+  Console.WriteLine("-> third number not");
+}
+*/
+
+
+//Задача 3: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Write("Input a number is day of the week: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int dayNumber) 
+{
+  if (dayNumber == 6 || dayNumber == 7) 
+  {
+  Console.WriteLine("(it is day off) -> yes");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) {
+    Console.WriteLine("it is not day of the week");
+  }
+  else Console.WriteLine("(it is not day off) -> no");
+}
